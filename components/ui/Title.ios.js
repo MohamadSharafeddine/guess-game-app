933c6eecd34213509f87677f72b99a168bd4841d
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-12-18 16:08:42 +0200

    Initial commit

---
+++ components/ui/Title.ios.js
@@ -0,0 +1,23 @@
+import { View, Text, StyleSheet, Platform } from "react-native";
+import React from "react";
+
+const Title = ({ children }) => {
+  return <Text style={styles.title}>{children}</Text>;
+};
+
+export default Title;
+
+const styles = StyleSheet.create({
+  title: {
+    fontFamily: "open-sans-bold",
+    fontSize: 24,
+    color: "white",
+    textAlign: "center",
+    // borderWidth: Platform.select({ ios: 0, android: 2 }),
+    borderWidth: 5,
+    borderColor: "white",
+    padding: 12,
+    maxWidth: "80%",
+    width: 300,
+  },
+});
---
+++ screens/StartGameScreen.js
@@ -0,0 +1,114 @@
+import {
+  View,
+  TextInput,
+  StyleSheet,
+  Alert,
+  Dimensions,
+  useWindowDimensions,
+  KeyboardAvoidingView,
+  ScrollView,
+} from "react-native";
+import React, { useState } from "react";
+import PrimaryButton from "../components/ui/PrimaryButton";
+import Colors from "../constants/colors";
+import Title from "../components/ui/Title.android";
+import Card from "../components/ui/Card";
+import InstructionText from "../components/ui/InstructionText";
+
+export default function StartGameScreen({ onPickNumber }) {
+  const [enteredNumber, setEnteredNumber] = useState("");
+
+  const { width, height } = useWindowDimensions();
+
+  function resetNumberHandler() {
+    setEnteredNumber("");
+  }
+
+  function numberInputHandler(enteredText) {
+    setEnteredNumber(enteredText);
+  }
+
+  function confirmInputHandler() {
+    const chosenNumber = parseInt(enteredNumber);
+
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert("Invalid Number", "Number must be between 1 and 99.", [
+        { text: "Okay", style: "destructive", onPress: resetNumberHandler },
+      ]);
+      return;
+    }
+
+    onPickNumber(chosenNumber);
+  }
+
+  const marginTop = height < 400 ? 30 : 100;
+
+  return (
+    <ScrollView style={styles.screen}>
+      <KeyboardAvoidingView style={styles.screen} behavior="position">
+        <View style={[styles.rootContainer, { marginTop: marginTop }]}>
+          <Title>Guess My Number</Title>
+          <Card style={styles.inputContainer}>
+            <InstructionText style={styles.instructionText}>
+              Enter a Number
+            </InstructionText>
+            <TextInput
+              style={styles.numberInput}
+              maxLength={2}
+              keyboardType="number-pad"
+              autoCapitalize="none"
+              autoCorrect={false}
+              value={enteredNumber}
+              onChangeText={numberInputHandler}
+            />
+            <View style={styles.buttonsContainer}>
+              <View style={styles.buttonContainer}>
+                <PrimaryButton onPress={resetNumberHandler}>
+                  Reset
+                </PrimaryButton>
+              </View>
+              <View style={styles.buttonContainer}>
+                <PrimaryButton onPress={confirmInputHandler}>
+                  Confirm
+                </PrimaryButton>
+              </View>
+            </View>
+          </Card>
+        </View>
+      </KeyboardAvoidingView>
+    </ScrollView>
+  );
+}
+
+const deviceHeight = Dimensions.get("window").height;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
+
+  rootContainer: {
+    flex: 1,
+    // marginTop: deviceHeight < 400 ? 30 : 200,
+    alignItems: "center",
+  },
+
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 24,
+    borderBottomColor: Colors.accent500,
+    borderBottomWidth: 2,
+    color: Colors.accent500,
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+
+  buttonsContainer: {
+    flexDirection: "row",
+  },
+  buttonContainer: {
+    flex: 1,
+  },
+});
